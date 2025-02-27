a62385fe63f17978792602c076d2d651260e2640
hiokky <hiokky.work@gmail.com>
2025-02-27 12:16:30 +0900

    Add colors

--- apps/studio.giselles.ai/tailwind.config.ts
+++ apps/studio.giselles.ai/tailwind.config.ts
@@ -39,6 +39,8 @@ const config = {
 				primary: {
 					DEFAULT: "hsl(var(--primary))",
 					foreground: "hsl(var(--primary-foreground))",
+					200: "hsl(var(--primary_200))",
+					100: "hsl(var(--primary_100))",
 				},
 				secondary: {
 					DEFAULT: "hsl(var(--secondary))",
@@ -64,7 +66,14 @@ const config = {
 					DEFAULT: "hsl(var(--card))",
 					foreground: "hsl(var(--card-foreground))",
 				},
+				error: {
+					900: "hsl(var(--error_900))",
+				},
+				warning: {
+					900: "hsl(var(--warning_900))",
+				},
 				black: {
+					800: "hsl(var(--black_800))",
 					400: "hsl(var(--black_400))",
 					100: "hsl(var(--black_100))",
 					80: "hsl(var(--black_80))",
@@ -79,6 +88,7 @@ const config = {
 				white: {
 					DEFAULT: "hsl(var(--white))",
 					900: "hsl(var(--white_900))",
+					850: "hsl(var(--white_850))",
 					400: "hsl(var(--white_400))",
 				},
 				green: {
@@ -86,6 +96,9 @@ const config = {
 				},
 				red: {
 					50: "hsl(var(--red_50))",
+				},
+				blue: {
+					80: "hsl(var(--blue_80))",
 				},
 				rosepine: {
 					base: "hsl(var(--rosepine_base))",
