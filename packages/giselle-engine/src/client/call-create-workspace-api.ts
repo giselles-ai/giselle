fa9cbfa5225ec5c2cae9e98d77b136fec3415133
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-03-04 19:01:31 +0900

    baseUrl

--- packages/giselle-engine/src/client/call-create-workspace-api.ts
+++ packages/giselle-engine/src/client/call-create-workspace-api.ts
@@ -1,12 +1,23 @@
 import { Workspace } from "@giselle-sdk/data-type";
 import { createWorkspace } from "../core/schema";
 
+function getBaseUrl() {
+	if (typeof window !== "undefined")
+		// browser should use relative path
+		return "";
+	if (process.env.VERCEL_URL)
+		// reference for vercel.com
+		return `https://${process.env.VERCEL_URL}`;
+	// assume localhost
+	return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export async function callCreateWorkspaceApi({
 	api = "/api/giselle/create-workspace",
 }: {
 	api?: string;
 } = {}) {
-	const response = await fetch(api, {
+	const response = await fetch(`${getBaseUrl()}${api}`, {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
