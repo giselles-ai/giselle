8d4052f19f916906b747042e5c1fb6b38400942b
Satoshi Ebisawa <satoshi.ebisawa@route06.co.jp>
2025-05-21 16:49:11 +0900

    feat(workflow-designer-ui): add support for query sources in InputPanel and related hooks

--- packages/text-editor/src/react/source-extension-react.tsx
+++ packages/text-editor/src/react/source-extension-react.tsx
@@ -1,6 +1,9 @@
 import { Node as GiselleNode } from "@giselle-sdk/data-type";
-import { type NodeViewProps, NodeViewWrapper, PasteRule } from "@tiptap/react";
-import { ReactNodeViewRenderer } from "@tiptap/react";
+import {
+	type NodeViewProps,
+	NodeViewWrapper,
+	ReactNodeViewRenderer,
+} from "@tiptap/react";
 import clsx from "clsx/lite";
 import { useMemo } from "react";
 import { SourceExtension } from "../extensions/source-extension";
@@ -16,6 +19,8 @@ function defaultName(node: GiselleNode) {
 					return node.name ?? node.content.provider;
 				case "action":
 					return node.name ?? node.content.command.provider;
+				case "query":
+					return node.name ?? "Query";
 				default: {
 					const _exhaustiveCheck: never = node.content;
 					throw new Error(`Unhandled action content type: ${_exhaustiveCheck}`);
@@ -63,6 +68,7 @@ const Component = (props: NodeViewProps) => {
 					"data-[content-type=file]:bg-node-data-900/20 data-[content-type=file]:text-node-data-900",
 					"data-[content-type=action]:bg-action-node-1/20 data-[content-type=action]:text-action-node-1",
 					"data-[content-type=trigger]:bg-trigger-node-1/20 data-[content-type=trigger]:text-trigger-node-1",
+					"data-[content-type=query]:bg-query-node-1/20 data-[content-type=query]:text-query-node-1",
 					"border-transparent data-[selected=true]:border-primary-900",
 					"text-[12px] ",
 				)}
