44f0ba620b34ddf0567b478a06d4c3b0b45d1a84
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-06-20 11:25:54 +0900

    Add example Sentry integration page and route
    The commit message should succinctly summarize the changes while
    following good Git style:
    
    ``` Add example Sentry integration page and route
    
    Adds: - Example page for testing Sentry error reporting - Test API route
    that throws errors - Updated Sentry version and configuration ```
    
    The message body provides useful additional context about what specific
    components were added, while keeping it concise and not repeating
    information from the subject line.

---
+++ apps/studio.giselles.ai/instrumentation-client.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://31ef4d348c8e420af1d1d8dcec3ab19e@o544600.ingest.us.sentry.io/4507535341387776",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
--- apps/studio.giselles.ai/next.config.ts
+++ apps/studio.giselles.ai/next.config.ts
@@ -80,7 +80,7 @@ const nextConfig: NextConfig = {
 
 export default withSentryConfig(nextConfig, {
 	// For all available options, see:
-	// https://github.com/getsentry/sentry-webpack-plugin#options
+	// https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
 	org: "route06cojp",
 	project: "edge",
@@ -94,20 +94,11 @@ export default withSentryConfig(nextConfig, {
 	// Upload a larger set of source maps for prettier stack traces (increases build time)
 	widenClientFileUpload: true,
 
-	// Automatically annotate React components to show their full name in breadcrumbs and session replay
-	reactComponentAnnotation: {
-		enabled: true,
-	},
-
 	// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
 	// This can increase your server load as well as your hosting bill.
 	// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
 	// side errors will fail.
-	// Turn it off for now. Will reconsider if issues arise.
-	// tunnelRoute: "/monitoring",
-
-	// Hides source maps from generated client bundles
-	hideSourceMaps: true,
+	tunnelRoute: "/monitoring",
 
 	// Automatically tree-shake Sentry logger statements to reduce bundle size
 	disableLogger: true,
