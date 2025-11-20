72a77d6b701c26291b537c2f168d10bb0e8406e6
satoshi toyama <satoshi.toyama@route06.co.jp>
2025-11-20 17:21:43 +0900

    Remove Perplexity language model

--- packages/language-model-registry/src/index.ts
+++ packages/language-model-registry/src/index.ts
@@ -3,11 +3,9 @@ export * from "./language-model";
 import { anthropic } from "./anthropic";
 import { google } from "./google";
 import { openai } from "./openai";
-import { perplexity } from "./perplexity";
 
 export const languageModels = {
 	...openai,
 	...anthropic,
 	...google,
-	...perplexity,
 };
