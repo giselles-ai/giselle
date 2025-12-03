481d3319c700fedea55f98c41090a20d431e9dd8
Tadashi Shigeoka <tadashi.shigeoka@route06.co.jp>
2025-12-03 17:29:50 +0900

    Update initialize-account to use isInternalUserEmail

--- apps/studio.giselles.ai/services/accounts/actions/initialize-account.ts
+++ apps/studio.giselles.ai/services/accounts/actions/initialize-account.ts
@@ -12,7 +12,7 @@ import {
 	users,
 	workspaces,
 } from "@/db";
-import { isEmailFromRoute06 } from "@/lib/utils";
+import { isInternalUserEmail } from "@/lib/utils";
 import { createTeamId } from "@/services/teams/utils";
 
 export const initializeAccount = async (
@@ -36,7 +36,7 @@ export const initializeAccount = async (
 			userDbId: user.dbId,
 			supabaseUserId,
 		});
-		const internalAccount = isEmailFromRoute06(supabaseUserEmail ?? "");
+		const internalAccount = isInternalUserEmail(supabaseUserEmail ?? "");
 		const [team] = await tx
 			.insert(teams)
 			.values({
