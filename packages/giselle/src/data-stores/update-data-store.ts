7661dcef8a9671ca580d6850f2be3d78aecd9b3c
Ryo Washizu <wasizuryo@gmail.com>
2026-01-14 12:50:58 +0900

    refactor(giselle): remove unnecessary Partial from updateDataStore

--- packages/giselle/src/data-stores/update-data-store.ts
+++ packages/giselle/src/data-stores/update-data-store.ts
@@ -10,7 +10,7 @@ export async function updateDataStore({
 }: {
 	context: GiselleContext;
 	dataStoreId: DataStoreId;
-	configuration: Partial<DataStore["configuration"]>;
+	configuration: DataStore["configuration"];
 }): Promise<DataStore> {
 	const path = dataStorePath(dataStoreId);
 
